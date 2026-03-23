834a594c8e46e624d3f324f66dc12a59eed92f09
AlexMani1501 <138869287+AlexMani1501@users.noreply.github.com>
2026-03-24 00:54:44 +0300

    improved the table ui + made table stub(zaglushka)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,34 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    ui->tableWidget->setColumnCount(7);
+    ui->tableWidget->setRowCount(5);
+
+    ui->tableWidget->setHorizontalHeaderLabels(
+        {"Year", "Region", "Growth", "Birth", "Death", "Weight", "Urban"});
+
+    QStringList regions = {
+        "Republic of Adygea",
+        "Moscow",
+        "Saint Petersburg",
+        "Kazan",
+        "Novosibirsk"
+    };
+
+    for (int row = 0; row < 5; row++)
+    {
+        ui->tableWidget->setItem(row, 0, new QTableWidgetItem(QString::number(2019 + row)));
+        ui->tableWidget->setItem(row, 1, new QTableWidgetItem(regions[row]));
+        ui->tableWidget->setItem(row, 2, new QTableWidgetItem(QString::number(-3.2 + row)));
+        ui->tableWidget->setItem(row, 3, new QTableWidgetItem(QString::number(10.1 + row)));
+        ui->tableWidget->setItem(row, 4, new QTableWidgetItem(QString::number(13.3 + row)));
+        ui->tableWidget->setItem(row, 5, new QTableWidgetItem(QString::number(1.2 + row)));
+        ui->tableWidget->setItem(row, 6, new QTableWidgetItem(QString::number(62 + row)));
+    }
+
+
 }
 
 MainWindow::~MainWindow()
